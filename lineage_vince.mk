af3321d0b1f8cbfc617124151c2d9b3cc9f38617
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-18 20:49:53 +0000

    vince: set build fingerprint props flags
    Signed-off-by: RennAlt <shenprjktplayground@gmail.com>

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -27,3 +27,9 @@ BUILD_USERNAME := RenzAlt
 BUILD_HOSTNAME := nightcord-at.25
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
