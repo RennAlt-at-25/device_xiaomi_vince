85cec8b154c1cf96a9ad39bdae33b94bd944f687
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-24 11:59:29 +0000

    vince: vendorsetup: change all source branch to lineage-22.2
    Signed-off-by: RennAlt <shenprjktplayground@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,34 +2,22 @@ git clone --depth=1 https://gitlab.com/nekoprjkt/aosp-clang -b 17 prebuilts/clan
 
 # Clone common tree if not exists
 if [ ! -d "device/xiaomi/mititanium-common" ]; then
-    git clone https://github.com/RenzAlt/android_device_xiaomi_mititanium-common -b 13 device/xiaomi/mititanium-common
+    git clone https://github.com/RenzAlt/android_device_xiaomi_mititanium-common -b lineage-22.2 device/xiaomi/mititanium-common
 else
     echo "device/xiaomi/mititanium-common already exists, skipping clone"
 fi
 
 # Clone proprietary if not exists
 if [ ! -d "vendor/xiaomi/vince" ]; then
-    git clone --depth=1 https://github.com/RenzAlt/vendor_xiaomi_vince -b 13 vendor/xiaomi/vince
+    git clone --depth=1 https://github.com/RenzAlt/vendor_xiaomi_vince -b lineage-22.2 vendor/xiaomi/vince
 else
     echo "vendor/xiaomi/vince already exists, skipping clone"
 fi
 
 if [ ! -d "vendor/xiaomi/mititanium-common" ]; then
-    git clone --depth=1 https://github.com/RenzAlt/proprietary_vendor_xiaomi_mititanium-common -b 13R vendor/xiaomi/mititanium-common
+    git clone --depth=1 https://github.com/RenzAlt/proprietary_vendor_xiaomi_mititanium-common -b lineage-22.2 vendor/xiaomi/mititanium-common
 else
     echo "vendor/xiaomi/mititanium-common already exists, skipping clone"
-fi
-
-if [ ! -d "vendor/xiaomi/mititanium-common-graphics" ]; then
-    git clone --depth=1 https://github.com/RenzAlt/proprietary_vendor_xiaomi_mititanium-common-graphics vendor/xiaomi/mititanium-common-graphics
-else
-    echo "vendor/xiaomi/mititanium-common-graphics already exists, skipping clone"
-fi
-
-if [ ! -d "vendor/xiaomi/mititanium-common-qc" ]; then
-    git clone --depth=1 https://github.com/RenzAlt/proprietary_vendor_xiaomi_mititanium-common-qc vendor/xiaomi/mititanium-common-qc
-else
-    echo "vendor/xiaomi/mititanium-common-qc already exists, skipping clone"
 fi
 
 if [ ! -d "kernel/xiaomi/msm8953" ]; then
@@ -45,13 +33,13 @@ else
 fi
 
 if [ ! -d "hardware/xiaomi" ]; then
-    git clone --depth=1 https://github.com/RennAlt/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+    git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 else
     echo "hardware/xiaomi already exists, skipping clone"
 fi
 
-if [ ! -d "vendor/extra" ]; then
-    git clone --depth=1 https://github.com/k4ngcaribug/vendor_extra -b ten vendor/extra
+if [ ! -d "vendor/lineage-priv" ]; then
+    git clone --depth=1 https://github.com/k4ngcaribug/vendor_extra -b 14 vendor/lineage-priv
 else
-    echo "vendor/extra already exists, skipping clone"
+    echo "vendor/lineage-priv already exists, skipping clone"
 fi
