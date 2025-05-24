41b66c80499fab5177b37a24f69a72c7f58c5053
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-24 11:48:14 +0000

    vince: switch to patchelf v0.18 for patching fingerprint blobs
    Co-authored-by: RennAlt <shenprjktplayground@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -8,8 +8,17 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/hw/gf_fingerprint.goodix.default.so)
+            "${PATCHELF_0_18}" --replace-needed "libvendor.goodix.hardware.fingerprint@1.0.so" "vendor.goodix.hardware.fingerprint@1.0.so" "${2}"
+            ;;
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
-            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            "${PATCHELF_0_18}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            "${PATCHELF_0_18}" --replace-needed "libvendor.goodix.hardware.fingerprint@1.0.so" "vendor.goodix.hardware.fingerprint@1.0.so" "${2}"
+            ;;
+        vendor/lib/libts_detected_face_hal.so)
+            ;&
+        vendor/lib/libts_face_beautify_hal.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
     esac
 }
