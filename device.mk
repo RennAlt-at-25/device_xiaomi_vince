0cb6cacf2346c5da19120c7604815a4f833af308
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-24 06:36:08 -0400

    vince: build biometrics HIDL
    Signed-off-by: RennAlt <shenprjktplayground@gmail.com>

--- device.mk
+++ device.mk
@@ -32,7 +32,10 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953 \
+    com.fingerprints.extension@1.0.vendor \
+    vendor.goodix.hardware.fingerprint@1.0.vendor \
+    liblzma.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
